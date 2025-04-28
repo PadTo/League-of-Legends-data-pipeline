907e4c2fc011fb872d6c127c3baafb2b9bd72ac0
Tomas Padegimas <tpadegimas4@gmail.com>
2025-04-28 12:04:57 +0200

    Renaming Filter Function

--- src/processing/LoLDatabaseQuery.py
+++ src/processing/LoLDatabaseQuery.py
@@ -4,8 +4,6 @@ from pathlib import Path
 # TODO: COMPLETELY CHANGE THIS
 
 
-class API_JsonResponseFilters:
+class DatabaseQuery:
     def __init__(self, path_to_database):
         self.path_to_database = path_to_database
-
-        pass
