d7147644ece39b10a6c4c3ff38dcc1ffcaaad8f3
Tomas Padegimas <tpadegimas4@gmail.com>
2025-05-10 11:55:00 +0200

    Fetching and Saving Data from DB
    Writing functions to save data to a csv file from the SQL database filtered by tier

--- src/processing/LoLDatabaseQuery.py
+++ src/processing/LoLDatabaseQuery.py
@@ -1,18 +1,17 @@
 from pathlib import Path
 import sqlite3
 import pandas as pd
-from logging_util.logging_setup import logging_setup
-import logging
 
 
 class DatabaseQuery:
     def __init__(self, path_to_database,
-                 logging_config_path,
+                 save_location=None,
                  col_to_exclude_from_data_participants=-1,
                  col_to_exclude_from_data_teams=-1,
                  col_to_exclude_from_data_timeline=-1):
 
         self.path_to_database = path_to_database
+        self.save_location = Path(save_location)
 
         self.match_data_participants_table_name = "Match_Data_Participants_Table"
         self.match_data_teams_table_name = "Match_Data_Teams_Table"
@@ -35,40 +34,94 @@ class DatabaseQuery:
             else col_to_exclude_from_data_timeline
         )
 
-        self.logger = logging.getLogger("DatabaseQueryLogging")
-        logging_setup(logging_config_path)
+    def _get_match_data(self, table_name, tier=-1, save=False):
+        try:
+            # Establish connection to the database
+            with sqlite3.connect(self.path_to_database) as connection:
+                cursor = connection.cursor()
 
-    def get_match_participant_data_by_tier(self, tier):
+                # Get column names from the table
+                cursor.execute(f"PRAGMA table_info({table_name})")
+                table_info = cursor.fetchall()
+                all_column_names = [row[1] for row in table_info]
+                print(table_info)
+                filtered_column_names = [
+                    col for col in all_column_names
+                    if col not in self.col_to_exclude_from_data_participants
+                ]
 
-        with sqlite3.connect(self.path_to_database) as connection:
-            cursor = connection.cursor()
-            cursor.execute(
-                f"PRAGMA table_info({self.match_data_participants_table_name})")
-            table_info = cursor.fetchall()
-            all_column_names = [row[1] for row in table_info]
-            filtered_column_names = [
-                col for col in all_column_names
-                if col not in self.col_to_exclude_from_data_participants]
+                # Prepare the column selection string
+                column_str = ",".join(filtered_column_names)
 
-            column_str = ",".join(filtered_column_names)
-            select_query = f'''
-                    SELECT {column_str}
-                    FROM {self.match_data_participants_table_name}
-                    WHERE gameTier == "{tier}"
+                # Select query based on tier value
+                if tier == -1:
+                    select_query = f'''
+                        SELECT {column_str}
+                        FROM {table_name}
                     '''
-            # cursor.execute(select_query)
-            data = pd.read_sql_query(select_query, connection)
+                    data_file_name = f"{table_name}_all_tiers"
+                else:
+                    select_query = f'''
+                        SELECT {column_str}
+                        FROM {table_name}
+                        WHERE gameTier == "{tier}"
+                    '''
+                    data_file_name = f"{table_name}_tier_{tier}"
 
-            print(data.head())
+                # Fetch the data
+                data = pd.read_sql_query(select_query, connection)
 
+                # Save data if requested
+                if save:
+                    if self.save_location is None:
+                        raise ValueError(
+                            "Save location must be included when saving datasets")
+                    abs_save_path = self.save_location / \
+                        f"{data_file_name}.csv"
+                    data.to_csv(abs_save_path)
 
-path = Path("D:\LoL Analysis Project\data")
+                return data
+        except Exception as e:
+            print(f"Error has occurred: {e}")
 
-path_db = path / "riot_data_database.db"
+    def get_match_participant_data_by_tier(self, tier=-1, save=False):
+        return self._get_match_data(self.match_data_teams_table_name, tier, save)
 
-pth = Path("D:\LoL Analysis Project\log_config\log_config_data_query.json")
+    def get_match_teams_data(self, tier=-1, save=False):
+        return self._get_match_data(self.match_data_participants_table_name, tier, save)
 
-DQ = DatabaseQuery(path_to_database=path_db, logging_config_path=pth)
+    def get_match_timeline_data_by_tier(self, tier=-1, save=False):
 
+        try:
+            with sqlite3.connect(self.path_to_database) as connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    f"PRAGMA table_info({self.match_timeline_table_name})")
+                table_info = cursor.fetchall()
 
-DQ.get_match_participant_data_by_tier("BRONZE")
+                all_column_names = [row[1] for row in table_info]
+                filtered_column_names = [
+                    col for col in all_column_names if col not in self.col_to_exclude_from_data_timeline]
+
+                column_str = ",".join(filtered_column_names)
+
+                if tier == -1:
+                    select_query = f'''
+                        SELECT {column_str}
+                        FROM {self.match_timeline_table_name}
+                    '''
+                    data_file_name = "match_timeline_data_all_tiers"
+                else:
+                    select_query = f'''
+                            SELECT {column_str}
+                            FROM {self.match_timeline_table_name} AS mt
+                            INNER JOIN Match_Data_Teams_Table AS mdt ON mdt.matchId = mt.matchId,
+                            WHERE mdt.gameTier = "{tier}"'''
+                    data_file_name = f"{self.match_timeline_table_name}_tier_{tier}"
+
+                data = pd.read_sql_query(select_query, connection)
+                save_path = self.save_location / f"{data_file_name}.csv"
+                data.to_csv(save_path)
+
+        except Exception as e:
+            print(f"Error has occurred: {e}")
