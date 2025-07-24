f6df1208f09694443243cc51ad2284c03e539024
PadTo <138981388+PadTo@users.noreply.github.com>
2025-07-24 15:50:26 +0100

    Intorducing rates constants for accessibility

---
+++ league_pipeline/constants/rates.py
@@ -0,0 +1,16 @@
+from enum import Enum
+import numpy as np
+
+class Rates(Enum):
+    MAX_CALLS = 100
+    WINDOW = 120
+
+    MAX_CALLS_PER_SECOND = 20
+    SECOND_WINDOW = 1
+
+    MAX_API_CALL_RETRIES = 30
+    MAX_WAITING_TIME_BETWEEN_RETRIES = 120
+
+    EXPONENTIAL_BACK_OFF_BASE_VALUE = np.e
+
+    
