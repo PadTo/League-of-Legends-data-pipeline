7cd7ff69cedf955700f105636df867a7821601b3
PadTo <138981388+PadTo@users.noreply.github.com>
2025-07-24 15:49:47 +0100

    Updating rate manager
    Introduced:
    sleep function
    logging

--- league_pipeline/rate_limiting/rate_manager.py
+++ league_pipeline/rate_limiting/rate_manager.py
@@ -1,12 +1,14 @@
 from league_pipeline.constants.rates import Rates
-from league_pipeline.constants.regions import Region
 import time
 import math
 from typing import Type
 from enum import Enum
+from logging import Logger
 
 class TokenBucket:
-    def __init__(self, regions: Type[Enum]) -> None:
+    def __init__(self, regions: Type[Enum], logger: Logger) -> None:
+
+        self.logger= logger
 
         slow_bucket_capacity: int = Rates.MAX_CALLS.value
         slow_bucket_tokens: int = Rates.MAX_CALLS.value
@@ -31,26 +33,46 @@ class TokenBucket:
             }
             self.token_bucket_regions[f"{region}"] = parameters 
         
+        self.elapsed_time_between_subsequent_calls = 0
+        
     
-    def _refill(self, region) -> None:
+    def _refill(self, region: str) -> None:
         now = time.monotonic()
         token_bucket_region = self.token_bucket_regions[region]
-        elapsed_time_between_calls = now - token_bucket_region["last_checked"]
+        self.elapsed_time_between_subsequent_calls = now - token_bucket_region["last_checked"]
         
-        refill_amount_slow: int = math.floor(token_bucket_region["slow_bucket_rate"] * elapsed_time_between_calls)
+        refill_amount_slow: int = math.floor(token_bucket_region["slow_bucket_rate"] * self.elapsed_time_between_subsequent_calls)
         token_bucket_region["slow_bucket_tokens"] = min(token_bucket_region["slow_bucket_capacity"], token_bucket_region["slow_bucket_tokens"] + refill_amount_slow)
 
-        refill_amount_fast: int = math.floor(token_bucket_region["fast_bucket_rate"] * elapsed_time_between_calls)
+        refill_amount_fast: int = math.floor(token_bucket_region["fast_bucket_rate"] * self.elapsed_time_between_subsequent_calls)
         token_bucket_region["fast_bucket_tokens"] = min(token_bucket_region["fast_bucket_capacity"], token_bucket_region["fast_bucket_tokens"] + refill_amount_fast)
 
         token_bucket_region["last_checked"] = now 
 
-    def allow_request(self, region) -> bool:
+        self.logger.info(
+                        f"[Refill] Region: {region} | Slow Tokens: {token_bucket_region['slow_bucket_tokens']} | Fast Tokens: {token_bucket_region['fast_bucket_tokens']}"
+                    )
+
+
+    def allow_request(self, region: str) -> bool:
         self._refill(region)
         token_bucket_region = self.token_bucket_regions[region]
         if token_bucket_region["fast_bucket_tokens"] and token_bucket_region["slow_bucket_tokens"] > 0:
             token_bucket_region["fast_bucket_tokens"] -= 1
             token_bucket_region["slow_bucket_tokens"] -= 1
+
+            self.logger.info(f"[ALLOW REQUEST] Region: {region} \n")
             return True
+        
+        self.logger.info(f"[DECLINE REQUEST] Region: {region} \n")
         return False
+    
+    def calculate_sleep_time(self, region: str) -> float:
+        sleep = self.token_bucket_regions[region]["slow_bucket_rate"] - self.elapsed_time_between_subsequent_calls 
+        
+        if sleep > 0:
+            self.logger.info(f"[COROUTINE] Region: {region} | Sleeping for {sleep} seconds \n")
+            return sleep
+        else:
+            return 0.0
             
