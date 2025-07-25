c8c3d80096e19ec9ccf3c37dd17f3b9e0b5957b7
PadTo <138981388+PadTo@users.noreply.github.com>
2025-07-25 14:55:38 +0100

    Updating leauge_ranks.py
    Added:
    GRANDMASTER tier
    additional comments
    variables for V5 endpoint

--- league_pipeline/constants/league_ranks.py
+++ league_pipeline/constants/league_ranks.py
@@ -2,21 +2,47 @@ from enum import Enum
 
 
 class RankedTier(Enum):
-    CHALLENGER = "CHALLENGER"
-    MASTER = "MASTER"
-    DIAMOND = "DIAMOND"
-    EMERALD = "EMERALD"
-    PLATINUM = "PLATINUM"
-    GOLD = "GOLD"
-    SILVER = "SILVER"
-    BRONZE = "BRONZE"
-    IRON = "IRON"
+    """
+    Represents the competitive ranked tiers in League of Legends, 
+    - Highest: CHALLENGER
+    - Lowest:  IRON
+    """
+    CHALLENGER  = "CHALLENGER"
+    GRANDMASTER = "GRANDMASTER"
+    MASTER      = "MASTER"
+    DIAMOND     = "DIAMOND"
+    EMERALD     = "EMERALD"
+    PLATINUM    = "PLATINUM"
+    GOLD        = "GOLD"
+    SILVER      = "SILVER"
+    BRONZE      = "BRONZE"
+    IRON        = "IRON"
 
 class RankedDivision(Enum):
-    I = "I"
-    II = "II"
+    """
+    Represents the divisions within most ranked tiers in League of Legends.
+    - Highest: Division I
+    - Lowest: Divison IV
+    Top tiers (CHALLENGER, GRANDMASTER and MASTER) do not have divisions.
+    """
+    I   = "I"
+    II  = "II"
     III = "III"
-    IV = "IV"
+    IV  = "IV"
 
 class RankedQueue(Enum):
+    """
+    Represents the queue types used in Riot's league-related endpoints 
+    (e.g., league-exp-v4). Currently includes only the classic solo ranked queue.
+    """
     RANKED_SOLO_5x5 = "RANKED_SOLO_5x5"
+
+class QueueMatchV5(Enum):
+    """
+    Represents broad classifications of match types used when querying
+    Riot’s Match V5 endpoints. These are custom groupings (not Riot queue IDs).
+    """
+    RANKED   = "ranked"    # Includes all ranked matches
+    NORMAL   = "normal"    # Includes all casual/unranked matches
+    TOURNEY  = "tourney"   # Includes Clash or other tournament-style matches
+    TUTORIAL = "tutorial"  # Includes tutorial or intro games
