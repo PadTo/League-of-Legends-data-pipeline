dff8ea94b396f8034c4c0c973901755aef61051f
PadTo <138981388+PadTo@users.noreply.github.com>
2025-07-25 14:55:58 +0100

    Visually Cleaning

--- league_pipeline/constants/rates.py
+++ league_pipeline/constants/rates.py
@@ -3,14 +3,16 @@ import numpy as np
 
 class Rates(Enum):
     MAX_CALLS = 100
-    WINDOW = 120
+    WINDOW    = 120
 
     MAX_CALLS_PER_SECOND = 20
-    SECOND_WINDOW = 1
+    SECOND_WINDOW        = 1
 
-    MAX_API_CALL_RETRIES = 30
+    MAX_API_CALL_RETRIES             = 30
     MAX_WAITING_TIME_BETWEEN_RETRIES = 120
 
+    # Exponential Back-Off Parameters
     EXPONENTIAL_BACK_OFF_BASE_VALUE = np.e
+    JITTER                          = True
 
     
