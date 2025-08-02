896724fb8c4e2a3c2c426568333ac14b8e33d9fb
PadTo <138981388+PadTo@users.noreply.github.com>
2025-08-02 12:31:22 +0100

    Update summoner.py
    Adding data transformation and additional functioanlity

--- league_pipeline/riot_api/summoner.py
+++ league_pipeline/riot_api/summoner.py
@@ -1,25 +1,32 @@
 from league_pipeline.constants.endpoints import *
-from typing import Type
 from logging import Logger
-from enum import Enum
 from aiohttp import ClientSession
-import asyncio
 from league_pipeline.utils.decorators import async_api_call_error_wrapper
 from league_pipeline.utils.exceptions import StatusResponseException
 from league_pipeline.rate_limiting.rate_manager import TokenBucket
 from league_pipeline.utils.http_utils import safely_fetch_rate_limited_data
+from league_pipeline.db.models import Summoners
+from league_pipeline.constants.regions import RegionMapping
+import datetime
 
 class SummonerEntries:
-    def __init__(self, api_key: str, regions: Type[Enum], logger: Type[Logger], token_bucket: TokenBucket) -> None:
+    def __init__(self, api_key: str, logger: Logger,
+                 token_bucket: TokenBucket) -> None:
+        
+        self.sql_table_object = Summoners
+
         self.api_key = api_key
         self.logger = logger
-        self.region_list = list(regions.__members__.keys())
+
         self.request_header = {"X-Riot-Token": api_key}
         self.status_response_exception = StatusResponseException()
         self.token_bucket = token_bucket
 
+
     @async_api_call_error_wrapper
-    async def summoner_entries_by_tier(self, region: str, queue: str, tier:str, division:str, pages: int, session: ClientSession) -> dict:
+    async def summoner_entries_by_tier(self, tier:str, queue: str, division:str,
+                                       pages:int, region: str, session: ClientSession,
+                                       transform_results: bool = True) -> list:
         """
         Fetches summoner entries by rank from the Riot API.
 
@@ -35,10 +42,33 @@ class SummonerEntries:
         Raises:
             StatusCodeError: If the API request fails or returns an error.
         """
-        summoner_entries_endpoint = LeagueEndpoint.ENTRIES_BY_TIER.value.format(queue=queue,tier=tier,division=division,pages=pages)
+
+        
+        summoner_entries_endpoint = LeagueEndpoint.ENTRIES_BY_TIER.value.format(queue=queue,tier=tier,
+                                                                                    division=division,pages=pages)
+        
         url = BaseEndpoint.BASE_RIOT_URL.value.format(region=region) + summoner_entries_endpoint
 
-        content = await safely_fetch_rate_limited_data(url, self.request_header,session,region,self.token_bucket,self.status_response_exception)
+        content = await safely_fetch_rate_limited_data(url, self.request_header,
+                                                       session,region,
+                                                       self.token_bucket,
+                                                       self.status_response_exception,
+                                                       logger=self.logger)
+        
+        for summoner in content:
+            try:
+                summoner["region"] = region
+            except Exception as e:
+                print(f"I have caused the issue {summoner}")
+                print(f"I have caused the issue {content}")
+                raise
+
+        
+
+        if transform_results:
+            transformed_results = self.transform_results(content)
+            return transformed_results
+
         return content
     
     @async_api_call_error_wrapper
@@ -59,7 +89,9 @@ class SummonerEntries:
         summoner_entries_endpoint = SummonerEndpoint.BY_PUUID.value.format(puuid)
         url = BaseEndpoint.BASE_RIOT_URL.value.format(region=region) + summoner_entries_endpoint
 
-        content = await safely_fetch_rate_limited_data(url,self.request_header,session,region,self.token_bucket,self.status_response_exception)
+        content = await safely_fetch_rate_limited_data(url,self.request_header,session,region,
+                                                       self.token_bucket,self.status_response_exception,
+                                                       logger=self.logger)
         
         if not content:
              return "UNRANKED"
@@ -70,10 +102,17 @@ class SummonerEntries:
              
         return "UNRANKED"
     
-    async def async_run_summoner_entries_by_tier(self,queue: str, tier:str, division:str, pages: int):
-
-        async with ClientSession() as session:
-           result = await asyncio.gather(*[self.summoner_entries_by_tier(region, queue, tier, division, pages,session) for region in self.region_list])
-
-        return result
-    
+    def transform_results(self, data: list) -> list:
+        transformed_data: list = []
+        current_date = str(datetime.datetime.now().date())
+        for result in data:
+            transformed_results = dict()
+            transformed_results["puuid"] = result["puuid"]
+            region = result["region"]
+            transformed_results["continental_region"] = RegionMapping.__members__[region].value
+            transformed_results["current_tier"] = result["tier"]
+            transformed_results["current_division"] = result["rank"]
+            transformed_results["date_collected"] = current_date
+            transformed_data.extend([transformed_results])
+        
+        return transformed_data
