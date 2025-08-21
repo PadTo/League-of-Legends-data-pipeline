c21186748d4c4df870fa73f30c2ff5110af60f7c
PadTo <138981388+PadTo@users.noreply.github.com>
2025-08-21 10:16:23 +0100

    Update file_folder_paths.py

--- league_pipeline/constants/file_folder_paths.py
+++ league_pipeline/constants/file_folder_paths.py
@@ -1,9 +1,13 @@
 from pathlib import Path
+from league_pipeline.constants.database_constants import DatabaseName
 
 class Paths:
     BASE = Path("D:/Programming Projects/League-of-Legends-data-pipeline")
     DATA = BASE / "data"
+    DATABASE = DATA / ".".join([str(DatabaseName.DATABASE_NAME.value),"db"])
     LEAGUE_PIPELINE = BASE / "league_pipeline"
     CONFIG = LEAGUE_PIPELINE / "config"
     KEY = LEAGUE_PIPELINE / "key" / "api_key.env"
     LOGGING_CONFIG = CONFIG / "log_config.json"
+    
+
