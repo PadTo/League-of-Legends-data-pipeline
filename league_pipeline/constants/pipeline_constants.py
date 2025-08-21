50ec1b4dcfa5aef7ed095aa1f7383f8b7ef62a82
PadTo <138981388+PadTo@users.noreply.github.com>
2025-08-21 10:16:29 +0100

    Update pipeline_constants.py

--- league_pipeline/constants/pipeline_constants.py
+++ league_pipeline/constants/pipeline_constants.py
@@ -1,12 +1,12 @@
 class Stages:
     TO_PROCESS = [1, 0, 0, 0, 0]
-    CLEAN_TABLES = None
-    DELETE_SUMMONERS_TABLE = False  
-
+    
+class EventTypes:
+    
    
 class DataProcessingConfig:
     PAGE_LIMIT = 2
-    DAY_LIMIT = 3          # In days
+    DAY_LIMIT = 10          # In days
     MATCHES_PER_TIER = 50
     PLAYERS_PER_TIER = 100
     START:int = 0
